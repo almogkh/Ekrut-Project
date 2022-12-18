a8423ab45f56943f8f9706acc5074cff280f9091
Yovel Gabbay <114578333+Yovelgabay@users.noreply.github.com>
2022-12-18 21:44:47 +0200

    Initialize server session manager, client session manager

--- EKrut-Client/src/ekrut/client/managers/ClientSessionManager.java
+++ EKrut-Client/src/ekrut/client/managers/ClientSessionManager.java
@@ -7,45 +7,57 @@ import ekrut.net.UserResponse;
 
 public class ClientSessionManager {
 	
-	private String sessionID;
-	//What is sessionID? Yovel
-	
-	public void loginUser(User user) throws Exception{
-		if(isLoggedin(user))
-			throw new IllegalArgumentException("User is already loggedin");
-		if (user.getUsername() == null || user.getPassword() == null)
-			throw new IllegalArgumentException("Null Item was provided");
-		
-		UserRequest userUpdateRequest = new UserRequest(UserRequestType.CONNECT, user.getUsername(),user.getPassword());
-		
-		UserResponse userUpdateResponse = sendRequest(userUpdateRequest);
-		if (userUpdateResponse.getResultCode().equals("OK")) 
-			return;
-		else
-			throw new IllegalArgumentException("Can't connect user");
-	}
-	
-	public void logoutUser(User user) {
-		UserRequest userUpdateRequest = new UserRequest(UserRequestType.DISCONNECT, user.getUsername());
-		UserResponse userUpdateResponse = sendRequest(userUpdateRequest);
-		if (userUpdateResponse.getResultCode().equals("OK")) 
-			return;
-		else
-			throw new IllegalArgumentException("Can't disconnect user");
-	}
-	
-	private boolean isLoggedin(User user) {
-		UserRequest userUpdateRequest = new UserRequest(UserRequestType.IS_CONNECTED, user.getUsername());
-		UserResponse userUpdateResponse = sendRequest(userUpdateRequest);
-		if (userUpdateResponse.getResultCode().equals("OK")) 
-			return true;
-		else {
-			throw new IllegalArgumentException("Can't disconnect user");
-		}
-	}
-	
+	private User user = null;	
 
-	private UserResponse sendRequest(UserRequest userUpdateRequest) {
+	public User loginUser(String username, String password) throws Exception{
+		
+		//Check if user is already logged in
+		if (user != null)
+			throw new RuntimeException("User is already loggedin");
+		
+		//Checking if the user has entered invalid details
+		else if (username == null || password == null)
+			throw new NullPointerException("Null Item was provided");
+		
+		//Send request to login user 
+		UserRequest userLoginRequest = new UserRequest(username, password);	
+		UserResponse userResponse = sendRequest(userLoginRequest);
+		
+		//If the response has confirmed return user details to know the next operations for this user
+		if (userResponse.getResultCode().equals("OK")) {
+			user = userResponse.getUser();
+			return user;
+		}
+		else
+			throw new RuntimeException(userResponse.getResultCode());
+	}
+
+	//Send to the server request to logout user
+	public void logoutUser(User user) throws Exception{
+		//Check if user is not loggedin
+		if (!isLoggedin()) {
+			throw new RuntimeException("User is not loggedin");
+		}
+		//Send request to logout user 
+		UserRequest userRequest = new UserRequest(UserRequestType.LOGOUT, user.getUsername());
+		UserResponse userResponse = sendRequest(userRequest);
+		
+		if (userResponse.getResultCode().equals("OK")) 
+			return;
+		else
+			throw new RuntimeException(userResponse.getResultCode());
+	}
+	
+	public boolean isLoggedin() {
+		UserRequest userRequest = new UserRequest(UserRequestType.IS_LOGGEDIN, user.getUsername());
+		UserResponse userResponse = sendRequest(userRequest);
+		if (userResponse.getResultCode().equals("OK")) 
+			return true;
+		return false;
+	}
+	
+	
+	private UserResponse sendRequest(UserRequest userLoginRequest) {
 		// TODO Auto-generated method stub
 		return null;
 	}
--- EKrut-Server/src/ekrut/server/managers/ServerSessionManager.java
+++ EKrut-Server/src/ekrut/server/managers/ServerSessionManager.java
@@ -1,5 +1,67 @@
 package ekrut.server.managers;
 
+import java.util.ArrayList;
+
+import ekrut.entity.User;
+import ekrut.net.UserResponse;
+import ekrut.server.db.UserDAO;
+
 public class ServerSessionManager {
+	
+	private User user = null;
+	private ArrayList<User> connectedUsers;
+	
+	public ServerSessionManager(ArrayList<User> connectedUsers, User user) {
+		this.connectedUsers = new ArrayList<User>();
+		this.user = user;
+	}
+
+	public UserResponse loginUser(String username, String password) {
+		String result = null;
+		user = UserDAO.fetchUserByUsername(username);
+		UserResponse userResponse = new UserResponse(result,user);
+		//Check if user not exist in DB
+		if (user == null) {
+			result = "Couldn't locate subscriber";
+		}
+		//if password isn't correct 
+		else if	(!user.getPassword().equals(password)){	
+			result = "Incorrect username or password";
+		}
+		//Else, add user to the connected users list, and return the connected user with response message OK
+		else {
+			userResponse.setUser(user);
+			connectedUsers.add(user);
+			result= "OK";
+		}
+		userResponse.setResultCode(result);
+		return userResponse;
+	}
+	
+	
+	public UserResponse logoutUser(String username) {
+		String result = null;
+		user = UserDAO.fetchUserByUsername(username);
+		UserResponse userResponse = new UserResponse(result);
+		//Check if user not exist in DB
+		if (user == null) {
+			result = "Couldn't locate subscriber";
+		}
+		else {
+			connectedUsers.remove(connectedUsers.indexOf(user));
+			result= "OK";
+		}
+		userResponse.setResultCode(result);
+		return userResponse;
+	}
+
+	public boolean isLoggedin(String username) {
+		user = UserDAO.fetchUserByUsername(username);
+		return connectedUsers.contains(user);
+	}
+
+	
+
+
 
 }
