c7b093fd81d961663a3d23f2f64e181cc3c023ac
Yovel Gabbay <114578333+Yovelgabay@users.noreply.github.com>
2022-12-18 21:43:00 +0200

    Intialize user classes

--- EKrut-Common/src/ekrut/net/UserRequest.java
+++ EKrut-Common/src/ekrut/net/UserRequest.java
@@ -3,8 +3,34 @@ package ekrut.net;
 import java.io.Serializable;
 
 public class UserRequest implements Serializable{
+	
 	private static final long serialVersionUID = 5395643634235579687L;
 	private UserRequestType action;
 	private String username;
 	private String password;
+	
+	//Constructor for connect action
+	public UserRequest(String username, String password) { 
+		this.action = UserRequestType.LOGIN;
+		this.username = username;
+		this.password = password;
+	}
+	
+	//Constructor for disconnect & isLoggedin actions
+	public UserRequest(UserRequestType action, String username) { 
+		this.action = action;
+		this.username = username;
+	}
+
+	public UserRequestType getAction() {
+		return action;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
 }
