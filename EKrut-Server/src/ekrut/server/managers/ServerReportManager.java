5d3852549e0da17f99070587278ee67ffcfe2dc3
Tal <talgaon4@gmail.com>
2022-12-20 17:51:43 +0200

    just self notes

--- EKrut-Server/src/ekrut/server/managers/ServerReportManager.java
+++ EKrut-Server/src/ekrut/server/managers/ServerReportManager.java
@@ -1,5 +1,13 @@
 package ekrut.server.managers;
 
 public class ServerReportManager {
-
+	public void generateReport() {
+		
+	}
+	// this method fetch all the relevant data to generate order report and process it into report
+	public void generateOrderReport() {
+		/* data to fetch from DB:
+		 * 	all the orders in the last month*/
+		
+	}
 }
