2cc78606a3cfe1a90da9bd3811c57c2dab9f7e99
Noymalka <Noy.malka@e.braude.ac.il>
2022-12-20 23:19:23 +0200

    Add to Ticket - threshold , itemID , itemName Change status from String to TicketStatus

--- EKrut-Common/src/ekrut/entity/Ticket.java
+++ EKrut-Common/src/ekrut/entity/Ticket.java
@@ -4,54 +4,80 @@ import java.io.Serializable;
 
 
 
-//Noy  
-
-
+/**
+ * 
+ * @author Noy Malka
+ *
+ */
 
 public class Ticket implements Serializable{
 	
 	private static final long serialVersionUID = -1064172186085363642L;
 	private int ticketId;
-	private String status;
+	private TicketStatus status;
 	private String ekrutLocation;
+	private int threshold;
+	private int itemID;
+	private String itemName;
 	
 	
-	public Ticket(int ticketId, String status, String ekrutLocation) {
-		
+	public Ticket(int ticketId, TicketStatus status, String ekrutLocation, int threshold, int itemID, String itemName) {
 		this.ticketId = ticketId;
 		this.status = status;
 		this.ekrutLocation = ekrutLocation;
+		this.itemID = itemID;
+		this.itemName = itemName;
+		this.threshold = threshold;
 	}
-	
+
 	public int getTicketId() {
 		return ticketId;
 	}
-
 
 	public void setTicketId(int ticketId) {
 		this.ticketId = ticketId;
 	}
 
-
-	public String getStatus() {
+	public TicketStatus getStatus() {
 		return status;
 	}
 
-
-	public void setStatus(String status) {
+	public void setStatus(TicketStatus status) {
 		this.status = status;
 	}
-
 
 	public String getEkrutLocation() {
 		return ekrutLocation;
 	}
 
-
 	public void setEkrutLocation(String ekrutLocation) {
 		this.ekrutLocation = ekrutLocation;
 	}
 
+	public int getItemID() {
+		return itemID;
+	}
 
+	public void setItemID(int itemID) {
+		this.itemID = itemID;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public int getThreshold() {
+		return threshold;
+	}
+
+	public void setThreshold(int threshold) {
+		this.threshold = threshold;
+	}
+	
+	
 	
 }
