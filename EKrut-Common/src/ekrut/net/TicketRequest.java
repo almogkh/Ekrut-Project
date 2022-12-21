f79caa061c3110ff718ad1c40e0678cc39220fca
Noymalka <Noy.malka@e.braude.ac.il>
2022-12-21 20:19:18 +0200

    Add ticketRequest

--- EKrut-Common/src/ekrut/net/TicketRequest.java
+++ EKrut-Common/src/ekrut/net/TicketRequest.java
@@ -2,10 +2,43 @@ package ekrut.net;
 
 import java.io.Serializable;
 
+import ekrut.entity.TicketStatus;
+
 public class TicketRequest implements Serializable{
+	
 	private static final long serialVersionUID = 7134346281040324590L;
 	private TicketRequestType action;
 	private int ticketId;
-	private String status;
+	private TicketStatus status;
 	private String ekrutLocation;
+	
+	
+	
+	public TicketRequest(TicketRequestType action, int ticketId) {
+		this.action = action;
+		this.ticketId = ticketId;
+	}
+
+	public TicketRequest(TicketRequestType action) {
+		this.action = action;
+	}
+
+	public TicketRequestType getAction() {
+		return action;
+	}
+
+	public int getTicketId() {
+		return ticketId;
+	}
+
+	public TicketStatus getStatus() {
+		return status;
+	}
+
+	public String getEkrutLocation() {
+		return ekrutLocation;
+	}
+
+	
+	
 }
