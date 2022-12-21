f13a51c147d82f1b9301c20d24212db50e52f96e
Noymalka <Noy.malka@e.braude.ac.il>
2022-12-21 12:50:07 +0200

    Add getters and consructors

--- EKrut-Common/src/ekrut/net/TicketRequest.java
+++ EKrut-Common/src/ekrut/net/TicketRequest.java
@@ -2,10 +2,55 @@ package ekrut.net;
 
 import java.io.Serializable;
 
+import ekrut.entity.TicketStatus;
+
+
+//initial TicketRequest 
+
+
 public class TicketRequest implements Serializable{
 	private static final long serialVersionUID = 7134346281040324590L;
 	private TicketRequestType action;
 	private int ticketId;
-	private String status;
+	private TicketStatus status;
 	private String ekrutLocation;
+	
+	
+	public TicketRequest(TicketRequestType action, String ekrutLocation) {
+		this.action = action;
+		this.ekrutLocation = ekrutLocation;
+	}
+
+	//Do we need also ticketID? 
+	public TicketRequest(TicketRequestType action, TicketStatus status) {
+		this.action = action;
+		this.status = status;
+	}
+
+	public TicketRequestType getAction() {
+		return action;
+	}
+
+	public int getTicketId() {
+		return ticketId;
+	}
+	
+	public TicketStatus getStatus() {
+		return status;
+	}
+
+	public String getEkrutLocation() {
+		return ekrutLocation;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- EKrut-Common/src/ekrut/net/TicketResponse.java
+++ EKrut-Common/src/ekrut/net/TicketResponse.java
@@ -3,6 +3,17 @@ package ekrut.net;
 import java.io.Serializable;
 
 public class TicketResponse implements Serializable{
+	
 	private static final long serialVersionUID = 5458389358466759549L;
 	private int resultCode;
+	
+	public TicketResponse(int resultCode) {
+		this.resultCode = resultCode;
+	}
+
+	public int getResultCode() {
+		return resultCode;
+	}	
+	
+	
 }
