6bb13c08a63ed58b04f3c824352a88602677ffca
Ofek Malka <ofek3113@gmail.com>
2022-12-25 20:32:05 +0200

    Add 'area' to InventoryItem entity.

--- EKrut-Common/src/ekrut/entity/InventoryItem.java
+++ EKrut-Common/src/ekrut/entity/InventoryItem.java
@@ -8,13 +8,15 @@ public class InventoryItem implements Serializable{
 	private Item item;
 	private int itemQuantity;
 	private String ekrutLocation;
+	private String area;
 	private int itemThreshold;
 	
-	public InventoryItem(Item item, int itemQuantity, String ekrutLocation, int itemThreshold) {
+	public InventoryItem(Item item, int itemQuantity, String ekrutLocation, String area, int itemThreshold) {
 		this.item = item;
 		this.itemQuantity = itemQuantity;
 		this.ekrutLocation = ekrutLocation;
 		this.itemThreshold = itemThreshold;
+		this.area = area;
 	}
 
 	public Item getItem() {
@@ -27,6 +29,10 @@ public class InventoryItem implements Serializable{
 	
 	public String getEkrutLocation() {
 		return ekrutLocation;
+	}
+	
+	public String getArea() {
+		return area;
 	}
 
 	public int getItemThreshold() {
