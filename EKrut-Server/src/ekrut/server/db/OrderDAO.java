66d311a8a7372e5867036dc5ff268121395f96bb
Nir-Betesh <Nirb1199@gmail.com>
2022-12-26 22:00:26 +0200

    Delete setString: unneeded.

--- EKrut-Server/src/ekrut/server/db/OrderDAO.java
+++ EKrut-Server/src/ekrut/server/db/OrderDAO.java
@@ -26,43 +26,44 @@ public class OrderDAO {
 	private DBController con;
 	private ItemDAO itemDAO;
 	private UserDAO userDAO;
+
 	/**
 	 * Constructs a new OrderDAO that uses the provided controller
 	 * 
-	 * @param con   the database controller to use
+	 * @param con the database controller to use
 	 */
 	public OrderDAO(DBController con) {
 		this.con = con;
 		this.itemDAO = new ItemDAO(con);
 		this.userDAO = new UserDAO(con);
 	}
-	
+
 	/**
 	 * Changes the default ItemDAO to use for item related database operations.
 	 * Useful for testing with mocks.
 	 * 
-	 * @param dao   the ItemDAO to use
+	 * @param dao the ItemDAO to use
 	 */
 	public void setItemDAO(ItemDAO dao) {
 		this.itemDAO = dao;
 	}
-	
+
 	/**
-	 * Creates a new order entry in the database and sets the order object's orderId field.
+	 * Creates a new order entry in the database and sets the order object's orderId
+	 * field.
 	 * 
-	 * @param order   the order that should be added to the database
-	 * @return        true if the operation succeeded, false otherwise
+	 * @param order the order that should be added to the database
+	 * @return true if the operation succeeded, false otherwise
 	 */
 	public boolean createOrder(Order order) {
 		con.beginTransaction();
-		
+
 		// Pass true so that we can get the new order ID
-		PreparedStatement p1 = con.getPreparedStatement("INSERT INTO orders " +
-                                                        "(date,status,type,dueDate,clientAddress,location,username) " +
-                                                        "VALUES(?,?,?,?,?,?,?)", true);
-		
-		PreparedStatement p2 = con.getPreparedStatement("INSERT INTO orderitems (orderId,itemId,itemQuantity) " +
-                                                        "VALUES(?,?,?)");
+		PreparedStatement p1 = con.getPreparedStatement("INSERT INTO orders "
+				+ "(date,status,type,dueDate,clientAddress,location,username) " + "VALUES(?,?,?,?,?,?,?)", true);
+
+		PreparedStatement p2 = con
+				.getPreparedStatement("INSERT INTO orderitems (orderId,itemId,itemQuantity) " + "VALUES(?,?,?)");
 		try {
 			p1.setObject(1, order.getDate(), MysqlType.DATETIME);
 			p1.setString(2, order.getStatus().toString());
@@ -78,26 +79,26 @@ public class OrderDAO {
 				p1.setNull(5, Types.VARCHAR);
 				p1.setString(6, order.getEkrutLocation());
 			}
-			
+
 			int count = con.executeUpdate(p1);
 			if (count != 1) {
 				con.abortTransaction();
 				return false;
 			}
-			
+
 			// Get the new order's ID
 			ResultSet rs = p1.getGeneratedKeys();
 			if (!rs.next()) {
 				con.abortTransaction();
 				return false;
 			}
-			
+
 			int orderId = rs.getInt(1);
 			rs.close();
-			
+
 			// Save the ID in the order object for later use
 			order.setOrderId(orderId);
-			
+
 			// Insert all the items in the order
 			for (OrderItem oi : order.getItems()) {
 				p2.setInt(1, orderId);
@@ -105,7 +106,7 @@ public class OrderDAO {
 				p2.setInt(3, oi.getItemQuantity());
 				p2.addBatch();
 			}
-			
+
 			int[] results = p2.executeBatch();
 			for (int i : results) {
 				if (i != 1) {
@@ -113,10 +114,10 @@ public class OrderDAO {
 					return false;
 				}
 			}
-			
+
 			con.commitTransaction();
 			return true;
-			
+
 		} catch (SQLException e) {
 			con.abortTransaction();
 			return false;
@@ -129,11 +130,11 @@ public class OrderDAO {
 			}
 		}
 	}
-	
+
 	private ArrayList<OrderItem> fetchOrderItems(int orderId) {
 		PreparedStatement p = con.getPreparedStatement("SELECT * FROM orderitems WHERE orderId = ?");
 		ArrayList<OrderItem> items = new ArrayList<>();
-		
+
 		try {
 			p.setInt(1, orderId);
 			ResultSet rs = p.executeQuery();
@@ -143,9 +144,9 @@ public class OrderDAO {
 				OrderItem orderItem = new OrderItem(item, rs.getInt(3));
 				items.add(orderItem);
 			}
-			
+
 			return items;
-			
+
 		} catch (SQLException e) {
 			return null;
 		} finally {
@@ -156,31 +157,31 @@ public class OrderDAO {
 			}
 		}
 	}
-	
+
 	/**
 	 * Returns the order with the specified ID.
 	 * 
-	 * @param orderId   the ID of the order we want to retrieve
-	 * @return          the desired order if found, null otherwise
+	 * @param orderId the ID of the order we want to retrieve
+	 * @return the desired order if found, null otherwise
 	 */
 	public Order fetchOrderById(int orderId) {
 		PreparedStatement p = con.getPreparedStatement("SELECT * FROM orders WHERE orderId = ?");
-		
+
 		try {
 			p.setInt(1, orderId);
 			ResultSet rs = p.executeQuery();
 			// No order with such an ID was found
-			if (!rs.next()) 
+			if (!rs.next())
 				return null;
-			
+
 			// Construct the main order object
 			Order order = new Order(rs.getInt(1), rs.getObject(2, LocalDateTime.class),
-					                OrderStatus.valueOf(rs.getString(3)), OrderType.valueOf(rs.getString(4)),
-					                rs.getObject(5, LocalDateTime.class), rs.getString(6), rs.getString(7), rs.getString(8));
-			
+					OrderStatus.valueOf(rs.getString(3)), OrderType.valueOf(rs.getString(4)),
+					rs.getObject(5, LocalDateTime.class), rs.getString(6), rs.getString(7), rs.getString(8));
+
 			order.setItems(fetchOrderItems(orderId));
 			return order;
-			
+
 		} catch (SQLException e) {
 			return null;
 		} finally {
@@ -191,32 +192,31 @@ public class OrderDAO {
 			}
 		}
 	}
-	
+
 	/**
 	 * Returns the orders that belong to the given user
 	 * 
 	 * @param username the username of the user whose orders should be fetched
-	 * @return         the list of orders that belong to the user
+	 * @return the list of orders that belong to the user
 	 */
 	public ArrayList<Order> fetchOrdersByUsername(String username) {
 		PreparedStatement p = con.getPreparedStatement("SELECT * FROM orders WHERE username = ?");
-		
+
 		try {
 			ResultSet rs = p.executeQuery();
 			ArrayList<Order> orders = new ArrayList<>();
-			
+
 			while (rs.next()) {
 				int orderId = rs.getInt(1);
 				Order order = new Order(orderId, rs.getObject(2, LocalDateTime.class),
-                                        OrderStatus.valueOf(rs.getString(3)), OrderType.valueOf(rs.getString(4)),
-                                        rs.getObject(5, LocalDateTime.class), rs.getString(6), rs.getString(7),
-                                        rs.getString(8));
+						OrderStatus.valueOf(rs.getString(3)), OrderType.valueOf(rs.getString(4)),
+						rs.getObject(5, LocalDateTime.class), rs.getString(6), rs.getString(7), rs.getString(8));
 				order.setItems(fetchOrderItems(orderId));
 				orders.add(order);
 			}
-			
+
 			return orders;
-			
+
 		} catch (SQLException e) {
 			return null;
 		} finally {
@@ -227,27 +227,27 @@ public class OrderDAO {
 			}
 		}
 	}
-	
+
 	/**
 	 * Updates the status of an order in the database.
 	 * 
-	 * @param orderId  the ID of the order whose status should be updated
-	 * @param status   the new order status to set
-	 * @return         true if the operation succeeded, false otherwise
+	 * @param orderId the ID of the order whose status should be updated
+	 * @param status  the new order status to set
+	 * @return true if the operation succeeded, false otherwise
 	 */
 	public boolean updateOrderStatus(int orderId, OrderStatus status) {
 		PreparedStatement p = con.getPreparedStatement("UPDATE orders SET status = ? WHERE orderId = ?");
-		
+
 		try {
 			p.setString(1, status.toString());
 			p.setInt(2, orderId);
-			
+
 			if (p.executeUpdate() != 1)
 				return false;
-			
+
 		} catch (SQLException e) {
 			return false;
-			
+
 		} finally {
 			try {
 				p.close();
@@ -255,34 +255,36 @@ public class OrderDAO {
 				throw new RuntimeException(e);
 			}
 		}
-		
+
 		return true;
 	}
 
-	
-	
 	// For Almog: Added by Nir, get list of orders in Shipment status.
-	public ArrayList<Order> fetchOrderShipmentListByArea(String area){
+	public ArrayList<Order> fetchOrderShipmentListByArea(String area) {
 		PreparedStatement ps = con.getPreparedStatement(
-				"SELECT orderId, date, status, type, dueDate, clientAddress, location, username"
-				+ "FROM orders"
-				+ "WHERE type = 'SHIPMENT'");
+				"SELECT o.orderId, o.date, o.status, o.type, o.dueDate, o.clientAddress, o.location, o.username "
+				+ "FROM orders o, ekrut_machines e "
+				+ "WHERE o.type = 'SHIPMENT'");
 		ArrayList<Order> orderList = new ArrayList<>();
 		try {
-			ps.setString(1, area);
+			// Get all orders that their status is SHIPMENT
 			ResultSet rs = con.executeQuery(ps);
-			while(rs.next()) {
-				User user = userDAO.fetchUserByUsername(rs.getString(7));		
+			while (rs.next()) {
+				// Get customer area in order to match the requested area with manager area.
+				User user = userDAO.fetchUserByUsername(rs.getString(7));
+				// if areas matches add the order to list.
 				if (user.getArea().equals(area)) {
 					orderList.add(new Order(rs.getInt(1), rs.getObject(2, LocalDateTime.class),
-			                OrderStatus.valueOf(rs.getString(3)), OrderType.valueOf(rs.getString(4)),
-			                rs.getObject(5, LocalDateTime.class), rs.getString(6), rs.getString(7), rs.getString(8)));
+							OrderStatus.valueOf(rs.getString(3)), OrderType.valueOf(rs.getString(4)),
+							rs.getObject(5, LocalDateTime.class), rs.getString(6), rs.getString(7), rs.getString(8)));
 				}
 			}
 			if (orderList.size() == 0)
 				return null;
+
+			return orderList;
 		} catch (SQLException e) {
-			e.printStackTrace();
+			return null;
 		} finally {
 			try {
 				ps.close();
@@ -290,10 +292,5 @@ public class OrderDAO {
 				throw new RuntimeException(e);
 			}
 		}
-		return orderList;	
 	}
 }
-
-
-
-
