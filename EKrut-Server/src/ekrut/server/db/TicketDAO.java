1e09e5937bc6f77671f16f367516f02c1b3cb198
Noymalka <Noy.malka@e.braude.ac.il>
2022-12-27 22:37:40 +0200

    Added fetchTicketsByUsername Updated new fields (area+username)

--- EKrut-Server/src/ekrut/server/db/TicketDAO.java
+++ EKrut-Server/src/ekrut/server/db/TicketDAO.java
@@ -46,14 +46,16 @@ public class TicketDAO {
 	public boolean createTicket(Ticket ticket) {
 		con.beginTransaction();
 		PreparedStatement ps = con.getPreparedStatement("INSERT INTO tickets " +
-	            "(status,location,threshold,itemID,itemName) " + "VALUES(?,?,?,?,?)",true);
+	            "(status,area,ekrutLocation,itemID,itemName,threshold,username) " + "VALUES(?,?,?,?,?,?,?)",true);
 		try {
 			
 			ps.setString(1, ticket.getStatus().toString());
-			ps.setString(2, ticket.getEkrutLocation());
-			ps.setInt(3, ticket.getThreshold());
+			ps.setString(2, ticket.getArea());
+			ps.setString(3, ticket.getEkrutLocation());
 			ps.setInt(4, ticket.getItemID());
 			ps.setString(5, ticket.getItemName());
+			ps.setInt(6, ticket.getThreshold());
+			ps.setString(7, ticket.getUsername());
 			int res= ps.executeUpdate();
 			if (res!=1) {
 				con.abortTransaction();
@@ -75,53 +77,23 @@ public class TicketDAO {
 
 	
 	/**
-	 * Retrieves a ticket from the database based on its ID.
-	 * 
-	 * @param ticketID the ID of the ticket to be retrieved
-	 * @return a Ticket object if the ticket was found, null otherwise
-	 * @throws RuntimeException if there was an error executing the SQL statement
-	 */
-	public Ticket fetchTicket(int ticketID) {
-		PreparedStatement ps = con.getPreparedStatement("SELECT * FROM tickets WHERE ticketID = ?;");
-		try {
-			ps.setInt(1, ticketID);
-			ResultSet rs = con.executeQuery(ps);
-			if(rs.next()) { 
-				return new Ticket(rs.getInt(1),TicketStatus.valueOf(rs.getString(2)), rs.getString(3),rs.getInt(4),rs.getInt(5),rs.getString(6)); 
-			}
-			return null;
-		} catch (SQLException e1) {
-			return null;
-		}finally {
-			try {
-				ps.close();
-			} catch (SQLException e) {
-				throw new RuntimeException(e);
-			}
-		}
-	}
-
-	
-	/**
-	 * Retrieves a list of tickets from the database based on their location.
+	 * Retrieves a list of tickets from the database based on their area.
 	 * 
 	 * @param location the location of the tickets to be retrieved
 	 * @return a list of Ticket objects if tickets were found, null otherwise
 	 * @throws RuntimeException if there was an error executing the SQL statement
 	 */
-	
-	
-	public ArrayList<Ticket> fetchTicketsByLocation(String location) {
-		ArrayList<Ticket> ticketsByLocation = new ArrayList<>();
-		PreparedStatement ps = con.getPreparedStatement("SELECT * FROM tickets WHERE location = ?;");
+	public ArrayList<Ticket> fetchTicketsByArea(String area) {
+		ArrayList<Ticket> ticketsByArea = new ArrayList<>();
+		PreparedStatement ps = con.getPreparedStatement("SELECT * FROM tickets WHERE area = ?;");
 		try {
-			ps.setString(1, location);
+			ps.setString(1, area);
 			ResultSet rs = con.executeQuery(ps);
 			while(rs.next()) { 
-				Ticket ticket = new Ticket(rs.getInt(1),TicketStatus.valueOf(rs.getString(2)), rs.getString(3),rs.getInt(4),rs.getInt(5),rs.getString(6));
-				ticketsByLocation.add(ticket);
+				Ticket ticket = new Ticket(rs.getInt(1),TicketStatus.valueOf(rs.getString(2)), rs.getString(3),rs.getString(4),rs.getInt(5),rs.getInt(6),rs.getString(7),rs.getString(8));
+				ticketsByArea.add(ticket);
 			}
-			return ticketsByLocation.size() != 0? ticketsByLocation : null;
+			return ticketsByArea.size() != 0? ticketsByArea : null;
 		} catch (SQLException e1) {
 			return null;
 		}finally {
@@ -132,6 +104,36 @@ public class TicketDAO {
 			}
 		}
 		
+	}
+	
+	
+	/**
+	 * Retrieves a list of tickets that are associated with the specified username (operation worker).
+	 *
+	 * @param username The username of the operation worker to search for.
+	 * @return A list of tickets that are associated with the specified username, or null if no tickets are found.
+	 * @throws SQLException if there is an error executing the SQL statement.
+	 */
+	public ArrayList<Ticket> fetchTicketsByUsername(String username) {
+		ArrayList<Ticket> ticketsByUserId = new ArrayList<>();
+		PreparedStatement ps = con.getPreparedStatement("SELECT * FROM tickets WHERE username = ?;");
+		try {
+			ps.setString(1, username);
+			ResultSet rs = con.executeQuery(ps);
+			while(rs.next()) { 
+				Ticket ticket = new Ticket(rs.getInt(1),TicketStatus.valueOf(rs.getString(2)), rs.getString(3),rs.getString(4),rs.getInt(5),rs.getInt(6),rs.getString(7),rs.getString(8));
+				ticketsByUserId.add(ticket);
+			}
+			return ticketsByUserId.size() != 0? ticketsByUserId : null;
+		} catch (SQLException e1) {
+			return null;
+		}finally {
+			try {
+				ps.close();
+			} catch (SQLException e) {
+				throw new RuntimeException(e);
+			}
+		}
 	}
 	
 	
@@ -167,6 +169,68 @@ public class TicketDAO {
 			
 			return true;
 		}
+
+	
+	
+	/**
+	 * Retrieves a list of facilities in a given area.
+	 *
+	 * @param area a string representing the area to search for facilities
+	 * @return an ArrayList of strings, each representing a facility in the given area.
+	 *         If no facilities are found or there is an error, the method returns null.
+	 */
+	public String fetchAreaByEkrutLocation(String ekrutLocation){
+
+		PreparedStatement ps1 = con.getPreparedStatement("SELECT area FROM ekrut_machines WHERE ekrutLocation = ?");
+		
+		try {
+			ps1.setString(1, ekrutLocation);
+			ResultSet rs1 = con.executeQuery(ps1);
+			String area;
+			if(!rs1.next()) {
+				return null;
+			}
+			area = rs1.getString("area");
+			
+			return area ;
+			
+			}catch (SQLException e) {
+				return null;
+			} finally { 
+				try {
+					ps1.close();
+				} catch (SQLException e) {
+					throw new RuntimeException(e);
+				}
+			}
+	}
+	
+//	/**
+//	 * Retrieves a ticket from the database based on its ID.
+//	 * 
+//	 * @param ticketID the ID of the ticket to be retrieved
+//	 * @return a Ticket object if the ticket was found, null otherwise
+//	 * @throws RuntimeException if there was an error executing the SQL statement
+//	 */
+//	public Ticket fetchTicket(int ticketID) {
+//		PreparedStatement ps = con.getPreparedStatement("SELECT * FROM tickets WHERE ticketID = ?;");
+//		try {
+//			ps.setInt(1, ticketID);
+//			ResultSet rs = con.executeQuery(ps);
+//			if(rs.next()) { 
+//				return new Ticket(rs.getInt(1),TicketStatus.valueOf(rs.getString(2)), rs.getString(3),rs.getInt(4),rs.getInt(5),rs.getString(6),rs.getString(7)); 
+//			}
+//			return null;
+//		} catch (SQLException e1) {
+//			return null;
+//		}finally {
+//			try {
+//				ps.close();
+//			} catch (SQLException e) {
+//				throw new RuntimeException(e);
+//			}
+//		}
+//	}
 
 	
 	
