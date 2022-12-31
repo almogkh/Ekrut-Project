9d700345f20e49965717fd67bd44b84ee7099264
Nir-Betesh <Nirb1199@gmail.com>
2022-12-31 13:55:53 +0200

    Initializ item in cart view.
    Design item in cart view.

---
+++ EKrut-Client/src/ekrut/client/gui/ItemInCartController.java
@@ -0,0 +1,52 @@
+package ekrut.client.gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ItemInCartController {
+
+    @FXML
+    private Button deleteItemBtn;
+
+    @FXML
+    private Label itemNameLbl;
+
+    @FXML
+    private Label itemPriceLbl;
+
+    @FXML
+    private Button minusBtn;
+
+    @FXML
+    private Button plusBtn;
+
+    @FXML
+    private TextField quantityTxt;
+
+    @FXML
+    private Button updateBtn;
+
+    @FXML
+    void deleteItem(ActionEvent event) {
+
+    }
+
+    @FXML
+    void minusItem(ActionEvent event) {
+
+    }
+
+    @FXML
+    void plusItem(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateItemQuantity(ActionEvent event) {
+
+    }
+
+}
