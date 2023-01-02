b252d23f7c377d7a2d8b0f3a1b5bf035b157a167
Almog Khaikin <almogkh@gmail.com>
2023-01-02 19:38:51 +0200

    Finish the Customer entity as we need to use it now
    Since the system requirements only state we need to worry about credit
    card *numbers*, I simplified the code to just have a string credit card
    number.

--- EKrut-Common/src/ekrut/entity/CreditCard.java
+++
@@ -1,9 +0,0 @@
-package ekrut.entity;
-
-import java.time.LocalDateTime;
-
-public class CreditCard {
-	private String cardNumber;
-	private LocalDateTime cardExpiration;
-	private String cvv;
-}
--- EKrut-Common/src/ekrut/entity/Customer.java
+++ EKrut-Common/src/ekrut/entity/Customer.java
@@ -1,10 +1,52 @@
 package ekrut.entity;
 
-public class Customer {
-	private int membershipNumber;
-	private int id;
-	private String firstName;
-	private String lastName;
-	private String phoneNumber;
-	private String email;
+import java.io.Serializable;
+
+public class Customer implements Serializable {
+
+	private static final long serialVersionUID = -2291478383986722760L;
+	
+	private String subscriberNumber;
+	private String username;
+	private String creditCardNumber;
+	private boolean monthlyCharge;
+
+	public Customer(String subscriberNumber, String username, boolean monthlyCharge, String creditCardNumber) {
+		this.subscriberNumber = subscriberNumber;
+		this.username = username;
+		this.creditCardNumber = creditCardNumber;
+		this.monthlyCharge = monthlyCharge;
+	}
+
+	public String getsubscriberNumber() {
+		return subscriberNumber;
+	}
+
+	public void setsubscriberNumber(String subscriberNumber) {
+		this.subscriberNumber = subscriberNumber;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getCreditCard() {
+		return creditCardNumber;
+	}
+
+	public void setCreditCard(String creditCardNumber) {
+		this.creditCardNumber = creditCardNumber;
+	}
+
+	public boolean isMonthlyCharge() {
+		return monthlyCharge;
+	}
+
+	public void setMonthlyCharge(boolean monthlyCharge) {
+		this.monthlyCharge = monthlyCharge;
+	}
 }
