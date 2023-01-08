9f8fc35b728d80ae07ee1955bc4ecbf84a55b909
Almog Khaikin <almogkh@gmail.com>
2023-01-08 12:26:44 +0200

    Don't use initializable as it is deprecated and unnecessary

--- EKrut-Client/src/ekrut/client/gui/ThresholdBrowserController.java
+++ EKrut-Client/src/ekrut/client/gui/ThresholdBrowserController.java
@@ -1,25 +1,22 @@
 package ekrut.client.gui;
 
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.ResourceBundle;
 import ekrut.client.EKrutClient;
 import ekrut.client.EKrutClientUI;
 import ekrut.client.managers.ClientInventoryManager;
 import ekrut.entity.InventoryItem;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.layout.VBox;
 
-public class ThresholdBrowserController implements Initializable{
+public class ThresholdBrowserController {
 
     @FXML
     private VBox thresholdsVbox;
 
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
+	@FXML
+	private void initialize() {
 		EKrutClient client = EKrutClientUI.getEkrutClient();
 		ClientInventoryManager CIM = client.getClientInventoryManager();
 		String currArea = client.getClientSessionManager().getUser().getArea();
