c277c2d598246b6400326f634d322129e8f2461a
Yovel Gabbay <114578333+Yovelgabay@users.noreply.github.com>
2023-01-10 14:58:26 +0200

    fix bug - get empty list

--- EKrut-Client/src/ekrut/client/gui/UserRegistrationController.java
+++ EKrut-Client/src/ekrut/client/gui/UserRegistrationController.java
@@ -9,6 +9,7 @@ import ekrut.entity.UserRegistration;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
 public class UserRegistrationController {
@@ -17,19 +18,27 @@ public class UserRegistrationController {
 
 	@FXML
 	private VBox usersContainerVbox;
+	
+
+    @FXML
+    private Label nullRegistrationList;
 
 	@FXML
 	private void initialize() {
 
 		client = EKrutClientUI.getEkrutClient();
 		clientSessionManager = client.getClientSessionManager();
-
+		nullRegistrationList.setVisible(false);
 		String userArea = client.getClientSessionManager().getUser().getArea();
 		ArrayList<UserRegistration> registrationList = clientSessionManager.getRegistrationList(userArea);
-
-		ObservableList<Node> registerContainerVboxChildren = usersContainerVbox.getChildren();
+		if (registrationList==null)
+			nullRegistrationList.setVisible(true);
+		else {
+			ObservableList<Node> registerContainerVboxChildren = usersContainerVbox.getChildren();
 		for (UserRegistration user : registrationList) {
 			registerContainerVboxChildren.add(new UserToRegisterController(user));
 		}
+		}
+		
 	}
 }
