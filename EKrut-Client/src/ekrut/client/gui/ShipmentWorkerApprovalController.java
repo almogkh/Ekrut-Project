f0e5d5ce0104b866d83110d5d640b65973517508
Nir-Betesh <Nirb1199@gmail.com>
2023-01-12 02:38:39 +0200

    Fixed code, i was change fetchShipmentByArea to get Shipments in Shipment type instad to get shipments that also SUBMITTED type in order to you this method in worker screen when he set done the orders.

--- EKrut-Client/src/ekrut/client/gui/ShipmentWorkerApprovalController.java
+++ EKrut-Client/src/ekrut/client/gui/ShipmentWorkerApprovalController.java
@@ -7,6 +7,7 @@ import ekrut.client.EKrutClientUI;
 import ekrut.client.managers.ClientSessionManager;
 import ekrut.client.managers.ClientShipmentManager;
 import ekrut.entity.Order;
+import ekrut.entity.OrderStatus;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -18,24 +19,35 @@ public class ShipmentWorkerApprovalController implements Initializable {
     @FXML
     private VBox ShipmentVBox;
     
-    private ArrayList<Order> ordersForShipping = new ArrayList<>();
     private ClientShipmentManager clientShipmentManager;
     private ClientSessionManager clientSessionManager;
   
  
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		ArrayList<Order> ordersForShipping = new ArrayList<>();
+		ArrayList<Order> orders = new ArrayList<>();
 		ArrayList<ShipmentWorkerAppController> ordersToShow = new ArrayList<>();
+		
 		clientShipmentManager = EKrutClientUI.getEkrutClient().getClientShipmentManager();
 		clientSessionManager = EKrutClientUI.getEkrutClient().getClientSessionManager();
 		String area = clientSessionManager.getUser().getArea();
 	
-		ordersForShipping = clientShipmentManager.fetchShipmentRequests(area);
+		orders = clientShipmentManager.fetchShipmentRequests(area);
 		
 		// Q.Nir - is needed or this 'for loop' can handle null array??
-		if (ordersForShipping == null)
+		if (orders.isEmpty())
 			return;
-
+		
+		for (Order order : orders) 
+			if (order.getStatus().equals(OrderStatus.SUBMITTED))
+				ordersForShipping.add(order);
+		
+		// Q.Nir - is needed or this 'for loop' can handle null array??
+		if (ordersForShipping.isEmpty())
+			return;
+		
+		
 		for (Order order : ordersForShipping) 
 			ordersToShow.add(new ShipmentWorkerAppController(order));
 		
