e07a41b5472e0ac09d639b7b4036f1f040566eef
Almog Khaikin <almogkh@gmail.com>
2023-01-17 20:48:01 +0200

    Small fix

--- EKrut-Client/src/ekrut/client/gui/OrderAddressInputController.java
+++ EKrut-Client/src/ekrut/client/gui/OrderAddressInputController.java
@@ -13,37 +13,37 @@ import javafx.scene.control.Alert.AlertType;
 
 public class OrderAddressInputController {
 
-  @FXML
-  private Button ContinueBtn;
+	@FXML
+	private Button ContinueBtn;
 
-  @FXML
-  private TextField addressTxt;
+	@FXML
+	private TextField addressTxt;
 
-  @FXML
-  private Button backBtn;
+	@FXML
+	private Button backBtn;
 
-  // Create order after
-  @FXML
-  void Continue(ActionEvent event) {
-    String address = addressTxt.getText();
-    if (address.equals(""))
-      new Alert(AlertType.ERROR, "You must enter address for shipment!.", ButtonType.OK).showAndWait();
-    else {
-      Optional<ButtonType> res = new Alert(AlertType.CONFIRMATION,
-          "Your address is: " + address + ", for continue in order press OK.", ButtonType.YES, ButtonType.NO)
-              .showAndWait();
-      res.ifPresent((btn) -> {
-        if (btn == ButtonType.YES) {
-          EKrutClientUI.getEkrutClient().getClientOrderManager().createOrder(address, true);
-          BaseTemplateController.getBaseTemplateController().switchStages("OrderItemBrowser");
-        }
-      });
-    }
-  }
+	// Create order after
+	@FXML
+	void Continue(ActionEvent event) {
+		String address = addressTxt.getText();
+		if (address.equals(""))
+			new Alert(AlertType.ERROR, "You must enter address for shipment!.", ButtonType.OK).showAndWait();
+		else {
+			Optional<ButtonType> res = new Alert(AlertType.CONFIRMATION,
+					"Your address is: " + address + ", for continue in order press OK.", ButtonType.YES, ButtonType.NO)
+							.showAndWait();
+			res.ifPresent((btn) -> {
+				if (btn == ButtonType.YES) {
+					EKrutClientUI.getEkrutClient().getClientOrderManager().createOrder(address, true);
+					BaseTemplateController.getBaseTemplateController().switchStages("OrderItemBrowser");
+				}
+			});
+		}
+	}
 
-  @FXML
-  void back(ActionEvent event) {
-    BaseTemplateController.getBaseTemplateController().switchStages("OrderCreation");
-  }
+	@FXML
+	void back(ActionEvent event) {
+		BaseTemplateController.getBaseTemplateController().switchStages("OrderCreation");
+	}
 
 }
