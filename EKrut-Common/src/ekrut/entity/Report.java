a73a06d6c1256faaa096b4fdd6c8e344a2009c90
Tal <talgaon4@gmail.com>
2023-01-22 16:13:25 +0200

    Add Inventory report equals

--- EKrut-Common/src/ekrut/entity/Report.java
+++ EKrut-Common/src/ekrut/entity/Report.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Objects;
 
 public class Report implements Serializable{
 
@@ -14,7 +15,7 @@ public class Report implements Serializable{
 	private LocalDateTime date;
 	private String area;
 	private String ekrutLocation;
-	
+
 	// Each data structure holds the relevant data to make a report
 	private Map<String, ArrayList<Integer>> inventoryReportData;
 	private Map<String, Integer> customerReportData;
@@ -22,7 +23,7 @@ public class Report implements Serializable{
 	private Map<String, Integer> topSellersData;
 	private Map<Integer, Integer> customersOrdersByDate;
 	
-	private Integer threshold;
+	private Integer threshold; 
 	private Integer totalOrders;
 	private Integer totalOrdersInILS;
 	private Integer totalShipmentOrders;
@@ -33,10 +34,10 @@ public class Report implements Serializable{
 		this.reportType = reportType;
 		this.date = date;
 		this.ekrutLocation = ekrutLocation;
-		this.area = area;
+		this.area = area; 
 	}
  
-	// Order report constructor
+	// Order report constructor 
 	public Report(
 			Integer reportID, ReportType reportType, LocalDateTime date,
 			String ekrutLocation, String area, Integer totalOrders, Integer totalOrdersInILS, Integer totalShipmentOrders, 
@@ -132,5 +133,27 @@ public class Report implements Serializable{
 
 	public void setReportType(ReportType reportType) {
 		this.reportType = reportType;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(area, date, ekrutLocation, inventoryReportData, reportID, reportType, threshold);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Report other = (Report) obj;
+		return Objects.equals(area, other.area) 
+				&& Objects.equals(date.toLocalDate(), other.date.toLocalDate()) 
+				&& Objects.equals(ekrutLocation, other.ekrutLocation)
+				&& Objects.equals(inventoryReportData, other.inventoryReportData)
+				&& Objects.equals(reportID, other.reportID)
+				&& reportType == other.reportType && Objects.equals(threshold, other.threshold);
 	}
 }
